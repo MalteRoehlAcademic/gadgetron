413ebee7318c3a32d904d2f3288f8aabb0f4c73b
Malte Roehl <m.roehl16@imperial.ac.uk>
2020-01-31 12:22:20 +0000

    test

--- toolboxes/python/python_IsmrmrdSpiralReconData_converter.h
+++ toolboxes/python/python_IsmrmrdSpiralReconData_converter.h
@@ -224,7 +224,7 @@ struct IsmrmrdReconDataSpiral_from_python_object {
 
     if (PyObject_HasAttrString(pyDataSpiral.ptr(),"result"))
     {
-      hoNDArray<std::complex<float>> test = bp::extract<hoNDArray<std::complex<float>>>(pyDataSpiral.attr("result"));
+      auto test = bp::extract<hoNDArray<std::complex<float>>>(pyDataSpiral.attr("result"));
       if (!test.get_data_ptr())
         result.result_ = bp::extract<hoNDArray<std::complex<float>>>(pyDataSpiral.attr("result"));
     }
